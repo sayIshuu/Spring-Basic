aa4fffc736adc55fdd6f098941fbbd2e2824b548
sayishuu <smile52673@gmail.com>
2023-08-01 21:03:34 +0900

    주석 추가 및 보조파일 추가

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 //컨트롤러 사용 예제
-    @GetMapping("hello")
+    @GetMapping("inja")
     public String hello(Model model){
         model.addAttribute("data", "hello!!");  // 모델에다가 data = "hello!!"라고 넣어놓은것.
         return "hello"; //get어노테이션에 의하여 resources 밑의 templates 밑에서 hello라는 이름의 파일을 찾는다.
